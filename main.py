58e3fccfad9ec7f846d85617937854c59c3258d6
Rohan Jain <75877962+captainjack0003@users.noreply.github.com>
2024-06-06 15:25:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,28 +7,34 @@ api_key_file = "api_key.txt"
 with open(api_key_file, "r") as file:
     api_key = file.read().strip()
 
-deployment_name='Enter Your Deployment Name on azure'
-azure_endpoint="enter your Azure End Point"
 
-connectionString = "Enter Your DB Connection String With user name and password"
-
-vector_store = ChromaDB_VectorStore()
-nosql_generator = NoSQLGenerator(connectionString)
-
-
-
-while True:
-
-    question=input("Enter Your Question here: ")
-    prompt=nosql_generator.extract_prompt(question)
-    token_len=nosql_generator.count_tokens(prompt)
+def main() :
+    deployment_name='Enter Your Deployment Name on azure'
+    azure_endpoint="enter your Azure End Point"
     
-    print("Your prompt Token Length: ",token_len)
-
-    query=nosql_generator.Azure_llm_Connection(api_key,azure_endpoint,deployment_name,prompt)
+    connectionString = "Enter Your DB Connection String With user name and password"
     
-    print("The Query Generated is: ",query)
-  
-    detax_cards_cursor=nosql_generator.run_query(query)
-    print("------------------------------------------------------------------")
-    print("result : ",list(detax_cards_cursor))
+    vector_store = ChromaDB_VectorStore()
+    nosql_generator = NoSQLGenerator(connectionString)
+    
+    
+    
+    while True:
+    
+        question=input("Enter Your Question here: ")
+        prompt=nosql_generator.extract_prompt(question)
+        token_len=nosql_generator.count_tokens(prompt)
+        
+        print("Your prompt Token Length: ",token_len)
+    
+        query=nosql_generator.Azure_llm_Connection(api_key,azure_endpoint,deployment_name,prompt)
+        
+        print("The Query Generated is: ",query)
+      
+        detax_cards_cursor=nosql_generator.run_query(query)
+        print("------------------------------------------------------------------")
+        print("result : ",list(detax_cards_cursor))
+
+
+if __name__ == "__main__":
+    main()
